721f5fe52413e86a801eea89ee60979250b04674
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2022-04-29 12:54:46 -0700

    Done lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,36 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesOnly1Elem() {
+        // prime
+        IntList lst = IntList.of(17);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("289", lst.toString());
+        assertTrue(changed);
+        // not prime
+        IntList lst2 = IntList.of(4);
+        boolean changed2 = IntListExercises.squarePrimes(lst);
+        assertEquals("4", lst2.toString());
+        assertTrue(!changed2);
+    }
+
+    @Test
+    public void testSquarePrimesWith0() {
+        // prime
+        IntList lst = IntList.of(0);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0", lst.toString());
+        assertTrue(!changed);
+    }
+
+    @Test
+    public void testSquareAllPrimes() {
+        // prime
+        IntList lst = IntList.of(17, 5, 3);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("289 -> 25 -> 9", lst.toString());
+        assertTrue(changed);
+    }
 }
